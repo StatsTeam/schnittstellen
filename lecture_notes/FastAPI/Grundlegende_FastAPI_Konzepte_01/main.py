0a3f26caee26e26b5b53fd456a60f3a5c54938b5
Olex <Olexandr@co-dex.de>
2025-02-16 13:13:24 +0100

    feat: add endpoint for darabase

--- lecture_notes/FastAPI/Grundlegende_FastAPI_Konzepte_01/main.py
+++ lecture_notes/FastAPI/Grundlegende_FastAPI_Konzepte_01/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 import models
 from models import Todos
 from database import engine, SessionLocal
@@ -21,3 +21,10 @@ db_dependency = Annotated[Session, Depends(get_db)]
 @app.get("/")
 async def read_all(db: db_dependency):
     return db.query(Todos).all()
+
+@app.get("/todo/{todo_id}")
+async def read_todo(db: db_dependency, todo_id: int):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code=404, detail="Todo not found.")
