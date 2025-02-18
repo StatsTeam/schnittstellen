18fd0f17dbb281f0922fd3836bf09160494f80f1
Olex <Olexandr@co-dex.de>
2025-02-18 18:42:11 +0100

    feat: add jwt description

--- lecture_notes/FastAPI/Grundlegende_FastAPI_Konzepte_01/routers/auth.py
+++ lecture_notes/FastAPI/Grundlegende_FastAPI_Konzepte_01/routers/auth.py
@@ -25,6 +25,10 @@ class CreateUserRequest(BaseModel):
     password: str
     role: str
     
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+    
 def get_db():
     db = SessionLocal()
     try:
@@ -66,11 +70,11 @@ async def create_user(db: db_dependency, create_user_request: CreateUserRequest)
     db.add(create_user_model)
     db.commit()
     
-@router.post("/token")
+@router.post("/token", response_model=Token)
 async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
     user: Users = authenticate_user(form_data.username, form_data.password, db)
     if not user:
         return "Failed Authentification"
     token = create_access_token(user.username, user.id, timedelta(minutes=20))
     
-    return token
+    return {"access_token": token, "token_type": "bearer"}
