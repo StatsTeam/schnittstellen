637823857da3494c78223b1fe49132f9aa9c1bba
Olex <Olexandr@co-dex.de>
2025-02-18 20:41:14 +0100

    feat: update endpoints with auth

--- lecture_notes/FastAPI/Grundlegende_FastAPI_Konzepte_01/routers/todos.py
+++ lecture_notes/FastAPI/Grundlegende_FastAPI_Konzepte_01/routers/todos.py
@@ -26,12 +26,17 @@ class TodoRequest(BaseModel):
     complete: bool
     
 @router.get("/", status_code=status.HTTP_200_OK)
-async def read_all(db: db_dependency):
-    return db.query(Todos).all()
+async def read_all(user: user_dependency, db: db_dependency):
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Authentification Failed")
+    return db.query(Todos).filter(Todos.owenr_id == user.get("id")).all()
 
 @router.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
-async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
-    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+async def read_todo(user: user_dependency, db: db_dependency, todo_id: int = Path(gt=0)):
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Authentification Failed")
+    
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).filter(Todos.owenr_id == user.get("id")).first()
     if todo_model is not None:
         return todo_model
     raise HTTPException(status_code=404, detail="Todo not found.")
