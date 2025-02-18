4e3d75e0692e3261dd7cfe9a20af44f465a7b593
Olex <Olexandr@co-dex.de>
2025-02-18 08:32:33 +0100

    feat: add authentification to app

--- lecture_notes/FastAPI/Grundlegende_FastAPI_Konzepte_01/routers/auth.py
+++ lecture_notes/FastAPI/Grundlegende_FastAPI_Konzepte_01/routers/auth.py
@@ -1,7 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from models import Users
 from passlib.context import CryptContext
+from database import SessionLocal
+from typing import Annotated
+from sqlalchemy.orm import Session
+from starlette import status
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter()
 bcrypt_context  = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -13,9 +18,26 @@ class CreateUserRequest(BaseModel):
     last_name: str
     password: str
     role: str
+    
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
+db_dependency = Annotated[Session, Depends(get_db)]
 
-@router.post("/auth/")
-async def create_user(create_user_request: CreateUserRequest):
+def authenticate_user(username: str, password: str, db: db_dependency):
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return True
+
+@router.post("/auth", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
     create_user_model = Users(
         email=create_user_request.email,
         username=create_user_request.username,
@@ -26,4 +48,12 @@ async def create_user(create_user_request: CreateUserRequest):
         is_active=True
     )
     
-    return create_user_model
+    db.add(create_user_model)
+    db.commit()
+    
+@router.post("/token")
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        return "Failed Authentification"
+    return "Successful Authentification"
